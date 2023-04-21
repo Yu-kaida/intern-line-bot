59e1276419908a14b8706d97e0d5355d3cf4e83f
gifteeintern2 <gifteeintern2@gifteeintern2noMacBook-Pro.local>
2023-04-21 14:21:06 +0900

    Level1

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ gem 'psych', '< 4'
 gem 'net-smtp', require: false
 gem 'net-imap', require: false
 gem 'net-pop', require: false
+
+gem 'dotenv-rails'
--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -24,17 +24,23 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          if event.message['text'].include?('おすすめ教えて')
+            message = {
+              type: 'text',
+              text: '今回のおすすめはこちらです'
+            }
+          else
+            message = {
+              type: 'text',
+              text: '「おすすめ教えて」と入力してください！'
+            }
+          end
           client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
           tf.write(response.body)
         end
-      end
     }
     head :ok
   end
